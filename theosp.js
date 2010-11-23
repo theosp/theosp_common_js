ed2086e1e7ccea306440c9020f46f8f4a79162f4
Daniel C <333222@gmail.com>
2010-11-23 15:01:03 +0200

    Apply jslint advices

--- theosp.js
+++ theosp.js
@@ -9,13 +9,13 @@
         };
 
     // based on jQuery's inArray
-    theosp.array.indexOf = function(elem, array) {
-        if ( array.indexOf ) {
-            return array.indexOf( elem );
+    theosp.array.indexOf = function (elem, array) {
+        if (array.indexOf) {
+            return array.indexOf(elem);
         }
 
-        for ( var i = 0, length = array.length; i < length; i++ ) {
-            if ( array[ i ] === elem ) {
+        for (var i = 0, length = array.length; i < length; i++) {
+            if (array[i] === elem) {
                 return i;
             }
         }
@@ -46,18 +46,19 @@
         return size;
     };
 
-    theosp.object.keys = Object.keys || function(o) {
+    theosp.object.keys = Object.keys || function (o) {
         var result = [];
-        for(var name in o) {
-            if (o.hasOwnProperty(name))
+        for (var name in o) {
+            if (o.hasOwnProperty(name)) {
                 result.push(name);
+            }
         }
         return result;
     };
 
-    theosp.object.deepKeys = Object.keys || function(o) {
+    theosp.object.deepKeys = Object.keys || function (o) {
         var result = [];
-        for(var name in o) {
+        for (var name in o) {
             result.push(name);
         }
         return result;
@@ -76,7 +77,7 @@
         var target = arguments[0] || {}, i = 1, length = arguments.length, deep = false, options, name, src, copy;
 
         // Handle a deep copy situation
-        if ( typeof target === "boolean" ) {
+        if (typeof target === "boolean") {
             deep = target;
             target = arguments[1] || {};
             // skip the boolean and the target
@@ -84,40 +85,40 @@
         }
 
         // Handle case when target is a string or something (possible in deep copy)
-        if ( typeof target !== "object" && !jQuery.isFunction(target) ) {
+        if (typeof target !== "object" && !jQuery.isFunction(target)) {
             target = {};
         }
 
         // extend jQuery itself if only one argument is passed
-        if ( length === i ) {
+        if (length === i) {
             target = this;
             --i;
         }
 
-        for ( ; i < length; i++ ) {
+        for (; i < length; i++) {
             // Only deal with non-null/undefined values
-            if ( (options = arguments[ i ]) != null ) {
+            if ((options = arguments[i]) != null) {
                 // Extend the base object
-                for ( name in options ) {
-                    src = target[ name ];
-                    copy = options[ name ];
+                for (name in options) {
+                    src = target[name];
+                    copy = options[name];
 
                     // Prevent never-ending loop
-                    if ( target === copy ) {
+                    if (target === copy) {
                         continue;
                     }
 
                     // Recurse if we're merging object literal values or arrays
-                    if ( deep && copy && ( jQuery.isPlainObject(copy) || jQuery.isArray(copy) ) ) {
-                        var clone = src && ( jQuery.isPlainObject(src) || jQuery.isArray(src) ) ? src
+                    if (deep && copy && (jQuery.isPlainObject(copy) || jQuery.isArray(copy))) {
+                        var clone = src && (jQuery.isPlainObject(src) || jQuery.isArray(src)) ? src
                             : jQuery.isArray(copy) ? [] : {};
 
                         // Never move original objects, clone them
-                        target[ name ] = jQuery.extend( deep, clone, copy );
+                        target[name] = jQuery.extend(deep, clone, copy);
 
                     // Don't bring in undefined values
-                    } else if ( copy !== undefined ) {
-                        target[ name ] = copy;
+                    } else if (copy !== undefined) {
+                        target[name] = copy;
                     }
                 }
             }
