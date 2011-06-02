d9998abed01372f7691e2af006fa77776a9f97e9
Daniel C <333222@gmail.com>
2011-06-02 15:12:16 +0300

    Add theosp.feature_detection.ie_version

--- theosp.js
+++ theosp.js
@@ -400,6 +400,30 @@
 
                 return o;
             }
+        },
+        // }}}
+
+        // feature_detection {{{
+        feature_detection: {
+
+            // ie_version {{{
+            ie_version: function(){
+                // https://gist.github.com/527683
+
+                var undef,
+                    v = 3,
+                    div = document.createElement('div'),
+                    all = div.getElementsByTagName('i');
+
+                while (
+                    div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+                    all[0]
+                );
+
+                return v > 4 ? v : undef;
+            }
+            // }}}
+
         }
         // }}}
 
