3f56f45fef7fbcf5cfdb2ddc70871cce593e211a
Daniel C <333222@gmail.com>
2011-07-10 13:44:11 +0300

    Make hash dispatcher general dispatcher

--- dispatcher.js
+++ dispatcher.js
@@ -1,5 +1,5 @@
 /*
- * HashDispatcher
+ * Dispatcher
  *
  * RERUIRES: Node.js's EventEmitter
  *           Daniel Chcouri's theosp_common_js (theosp.js)
@@ -45,10 +45,10 @@ if ((typeof $ === "undefined" || typeof $.extend === "undefined") && typeof requ
 // }}}
 
 (function () {
-    // HashDispatcher {{{
+    // Dispatcher {{{
 
     // constructor {{{
-    var HashDispatcher = function (routes, options) {
+    var Dispatcher = function (routes, options) {
         var self = this;
 
         // init options {{{
@@ -65,47 +65,35 @@ if ((typeof $ === "undefined" || typeof $.extend === "undefined") && typeof requ
         }
         self.routes = routes;
         // }}}
-
-        // If running under a browser bind to the hashchange event {{{
-        if (typeof window !== 'undefined') {
-            $(window).bind('hashchange', function() {
-                self.dispatch();
-            });
-
-            $(window).ready(function () {
-                $(window).trigger('hashchange');
-            });
-        }
-        // }}}
     };
     // }}}
 
     // Prototypical Chain {{{
-    HashDispatcher.prototype = new EventEmitter();
-    HashDispatcher.prototype.constructor = HashDispatcher;
+    Dispatcher.prototype = new EventEmitter();
+    Dispatcher.prototype.constructor = Dispatcher;
     // }}}
 
     // properties {{{
-    $.extend(HashDispatcher.prototype, {
+    $.extend(Dispatcher.prototype, {
         // options {{{
-        options: { }
+        options: { 
+            default_dispatcher_input: ""
+        }
         // }}}
     });
     // }}}
 
     // methods {{{
-    $.extend(HashDispatcher.prototype, {
+    $.extend(Dispatcher.prototype, {
         // dispatch {{{
-        dispatch: function (hash) {
+        dispatch: function (string) {
             var self = this;
 
-            // If running under a browser use the current hash as default
-            // otherwise use "#" {{{
-            if (typeof hash === 'undefined') {
-                if (typeof window !== 'undefined') {
-                    hash = window.location.hash;
+            if (typeof string === 'undefined') {
+                if (typeof self.options.default_dispatcher_input === "function") {
+                    string = self.options.default_dispatcher_input();
                 } else {
-                    hash = "#";
+                    string = self.options.default_dispatcher_input;
                 }
             }
 
@@ -115,14 +103,14 @@ if ((typeof $ === "undefined" || typeof $.extend === "undefined") && typeof requ
                 if (typeof route.regex !== 'undefined') {
                     // avoid bugs in cases where the regex ends with /g
                     route.regex.lastIndex = 0;
-                    if ((route_params = route.regex.exec(hash)) !== null) {
+                    if ((route_params = route.regex.exec(string)) !== null) {
                         route.action.apply(this, route_params);
 
                         return;
                     }
                 } else {
                     // We look on routes without route.regex as defaults
-                    route.action(hash);
+                    route.action(string);
 
                     return;
                 }
@@ -138,10 +126,10 @@ if ((typeof $ === "undefined" || typeof $.extend === "undefined") && typeof requ
     // export {{{
     if (typeof module === 'object' && typeof module.exports === 'object') {
         // If imported using common js (nodejs)
-        exports.HashDispatcher = HashDispatcher;
+        exports.Dispatcher = Dispatcher;
     } else {
         // Otherwise simply attach to the global object
-        this.HashDispatcher = HashDispatcher;
+        this.Dispatcher = Dispatcher;
     }
     // }}}
 
